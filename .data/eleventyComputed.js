1b84ae2dea9a21da5ce06e64dfef52911e633540
Sam Goto <goto@chromium.org>
2021-09-21 15:52:59 -0700

    Port the explainer to eleventy

---
+++ .data/eleventyComputed.js
@@ -0,0 +1,3 @@
+module.exports = {
+  permalink: "{{ page.inputPath | replace }}.html"
+};
--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,51 @@
 // const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
-  // eleventyConfig.setTemplateFormats(["html", "md", "xml"]);
+  const md = require("markdown-it")({
+    html: true,
+    breaks: true,
+    linkify: false
+  });
 
+  var defaultRender = md.renderer.rules.link_open || function(tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+  md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+    const href = tokens[idx].attrIndex('href');
+
+    if (href < 0) {
+      return defaultRender(tokens, idx, options, env, self);
+    }
+    
+    const url = tokens[idx].attrs[href][1];
+    if (url.endsWith(".md")) {
+      const result = url.replace(".md", ".html")
+      tokens[idx].attrs[href][1] = result;
+    }
+
+    return defaultRender(tokens, idx, options, env, self);
+  };
+  
+  // var defaultRender = md.renderer.rules.image;
+  
+  eleventyConfig.setLibrary("md", md);
+  
   // Copy the `assets` directory to the compiled site folder
   eleventyConfig.addPassthroughCopy("static");
   //eleventyConfig.addPassthroughCopy("tmp");
 
   // eleventyConfig.addPlugin(syntaxHighlight);
+
+  eleventyConfig.addFilter("replace", function(value) {
+    if (value.endsWith(".md")) {
+      return value.replace(".md", "");
+    }
+    if (value.endsWith(".html")) {
+      return value.replace(".html", "");
+    }
+    return value;
+  });
 
   return {
     dir: {
